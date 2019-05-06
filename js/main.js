58e529a11f762c31ef4596c6415ca652ae16570f
Felipe Corrêa <felipecorrea@users.noreply.github.com>
2019-05-06 11:52:41 -0300

    Novas férias! ALELUIA!!!

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 07, 2018 17:00:00",
+	date: "June 7, 2019 18:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
